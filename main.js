5e54119771e8bab6a85d042c941908828eacb344
Aaron Miller <39493031+millerac333@users.noreply.github.com>
2018-09-15 22:02:25 -0500

    update main.js
    wrote additional way to solve issue in README.md below my original solution

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
-//creates FOR-loop that will consecutively calls intergers 1 through 100
-for (let i = 1; i <= 100; i++) {
-    const chickenNumber = [i];
+// //creates FOR-loop that will consecutively calls intergers 1 through 100
+// for (let i = 1; i <= 100; i++) {
+//     const chickenNumber = [i];
 
-    //Test FOR-loop
-    // console.log(chickenNumber);
+//     //Test FOR-loop
+//     // console.log(chickenNumber);
 
-    // IF function that iterates through values in loop finding the numbers divisible by 5 AS WELL AS 7 then replacing value with "string"
-    if (chickenNumber % 5 === 0 && chickenNumber % 7 === 0) {
-        console.log("Chicken Monkey");    
-    } 
+//     // IF function that iterates through values in loop finding the numbers divisible by 5 AS WELL AS 7 then replacing value with "string"
+//     if (chickenNumber % 5 === 0 && chickenNumber % 7 === 0) {
+//         console.log("Chicken Monkey");    
+//     } 
     
-    //if above condition not met, this function will find the numbers in loop divisbile by 5 and replace value with "string"
-    else if (chickenNumber % 5 === 0) {
-        console.log("Chicken");
-    } 
+//     //if above condition not met, this function will find the numbers in loop divisbile by 5 and replace value with "string"
+//     else if (chickenNumber % 5 === 0) {
+//         console.log("Chicken");
+//     } 
 
-    //if above conditions not met, this function will find the numbers in loop divisbile by 7 and replace value with "string"
-    else if (chickenNumber % 7 === 0) {
-        console.log("Monkey");
-    } 
+//     //if above conditions not met, this function will find the numbers in loop divisbile by 7 and replace value with "string"
+//     else if (chickenNumber % 7 === 0) {
+//         console.log("Monkey");
+//     } 
 
-    // otherwise console.log originial value of loop
-    else {
-        console.log(chickenNumber);
-    }
+//     // otherwise console.log originial value of loop
+//     else {
+//         console.log(chickenNumber);
+//     }
+// }
+
+// Additional Way
+
+for (let i = 1; i <= 100; i++) {
+  let numToString = "";
+  if (i % 5 === 0) {
+    numToString += "Chicken";
+  }
+  if (i % 7 === 0) {
+    numToString += "Monkey";
+  }
+  if (numToString === "") {
+    numToString = i;
+  }
+  console.log(numToString);
 }
 
